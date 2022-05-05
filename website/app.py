8217cfa3db35ef30f3bd7983e6b3f6d6eb7c7942
corentinleroux <20160279@etud.univ-evry.fr>
2022-05-05 13:31:59 +0200

    Website UI fix

--- website/app.py
+++ website/app.py
@@ -5,6 +5,10 @@ import pickle
 import numpy as np
 from IPython.display import display
 import sys
+import random 
+
+divorce = ["You should have listen to your mom. Divorce", "Sorry to say but their is no chance for you. Divorce"]
+not_divorce = ["You should stay together", "All fine, stay together", "How dare you have any doubts"]
 
 app = Flask(__name__)
 if __name__ == '__main__':
@@ -20,13 +24,13 @@ def home():
         to_predict_list = list(map(int, to_predict_list))
         to_predict = np.array(to_predict_list).reshape(1, 54)
         display(to_predict)
-        loaded_model = joblib.load(open("model/bagging_classifier.pkl", 'rb'))
+        loaded_model = joblib.load(open("model/bagging_extratrees.pkl", 'rb'))
         result = loaded_model.predict(to_predict)[0]
         print(result)
         if result == 1 or result =="1" :
-          prediction = "You should divorce"
+          prediction = random.choice(divorce)
         else :
-          prediction = "All fine, you can stay together"
+          prediction = random.choice(not_divorce)
         
     else:
         prediction = ""
